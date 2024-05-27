6cb23dbb880c53a59a0fb955c0819d075b146bbc
morkmorquecho <105129572+morkmorquecho@users.noreply.github.com>
2024-05-27 17:40:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000; // Puedes cambiar el puerto si es necesario
+const port = 8081; // Puedes cambiar el puerto si es necesario
 
 // Define el directorio de los archivos estáticos
 app.use(express.static(path.join(__dirname, 'pagina web')));
